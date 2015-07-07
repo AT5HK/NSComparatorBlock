a1dbe4b7691d95ce677757bccb86d8d2b976ccb1
Auston <auston.salvana@live.com>
2015-07-07 11:06:22 -0700

    first 1 done

--- Arrays/main.m
+++ Arrays/main.m
@@ -11,7 +11,33 @@
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
-        NSLog(@"Hello, World!");
+        NSArray *arrayOfStuff = @[
+                                  @"eeeeeAAbfdf",
+                                  @"Everything",
+                                  @"news to me",
+                                  @"timepiece",
+                                  @"egg",
+                                  @"eggs",
+                                  @"paper",
+                                  @"e",
+                                  @"facebook",
+                                  @"painting",
+                                  @"231 thurlow st",
+                                  @"99 bottles"
+                                  ];
+        
+        NSArray *sortedArrayOfString = [arrayOfStuff sortedArrayUsingComparator:^NSComparisonResult(id obj1, id obj2) {
+            
+            return [(NSString *)obj1 compare:(NSString *)obj2 options:NSNumericSearch];
+        }];
+        
+        NSArray *sortLength = [arrayOfStuff sortedArrayUsingComparator:^NSComparisonResult(id obj1, id obj2) {
+            if ([obj1 length] > [obj2 length]) {
+                return (NSComparisonResult)obj1;
+            }
+            return (NSComparisonResult)obj2;
+        }];
+    
     }
     return 0;
 }
