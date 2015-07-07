9f0bac0e787bf6b9a135eb90ca24f87da75057fc
Auston <auston.salvana@live.com>
2015-07-07 11:51:18 -0700

    done

--- Arrays/main.m
+++ Arrays/main.m
@@ -30,14 +30,26 @@ int main(int argc, const char * argv[]) {
             
             return [(NSString *)obj1 compare:(NSString *)obj2 options:NSNumericSearch];
         }];
-        
+        NSLog(@"By numeric: %@", sortedArrayOfString);
         NSArray *sortLength = [arrayOfStuff sortedArrayUsingComparator:^NSComparisonResult(id obj1, id obj2) {
-            if ([obj1 length] > [obj2 length]) {
-                return (NSComparisonResult)obj1;
-            }
-            return (NSComparisonResult)obj2;
+            return (NSComparisonResult)[obj1 length] < [obj2 length];
         }];
-    
+        NSLog(@"By Length: %@", sortLength);
+        
+            NSArray *lastChar = [arrayOfStuff sortedArrayUsingComparator:^NSComparisonResult(id obj1, id obj2) {
+                NSString *character1 = [obj1 substringFromIndex:[obj1 length] -1];
+                NSString *character2 = [obj2 substringFromIndex:[obj2 length] -1];
+                return [character1 compare:character2 options:NSNumericSearch];
+            }];
+        NSLog(@"By last charachter alphabetical: %@", lastChar);
+        
+        NSArray *sortByE = [arrayOfStuff sortedArrayUsingComparator:^NSComparisonResult(id obj1, id obj2) {
+            NSArray *eArray =[obj1 componentsSeparatedByString:@"e"];
+            NSArray *eArray2 = [obj2 componentsSeparatedByString:@"e"];
+            
+            return (NSComparisonResult)[eArray count] < [eArray2 count];
+        }];
+        NSLog(@"By number of Es: %@", sortByE);
     }
     return 0;
 }
