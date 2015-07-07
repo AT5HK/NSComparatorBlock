8a591fc52c55838870c1ae07d2dd60b0ed00ee87
Auston <auston.salvana@live.com>
2015-07-07 10:21:43 -0700

    Initial Commit

---
+++ Arrays/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Arrays
+//
+//  Created by Auston Salvana on 7/7/15.
+//  Copyright (c) 2015 ASolo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
